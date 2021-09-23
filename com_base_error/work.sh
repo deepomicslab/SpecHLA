30acfdce6bbbc3d3777d4a44663fdb448940fd5d
WANG Shuai <wshuai294@gmail.com>
2021-09-23 18:10:44 +0800

    init

--- com_base_error/work.sh
+++
@@ -1,6 +0,0 @@
-dir=/mnt/d/HLAPro_backup/HLAPro/com_base_error/
-bash $dir/run.blast.baseerror.sh $1
-perl $dir/merge.novel.baseerror1.pl
-perl $dir/merge.novel.baseerror2.pl
-perl $dir/count_fre2.pl
-
