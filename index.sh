58abeebb56a4cba6ae7af36d59290b883a101e2b
WANG Shuai <wshuai294@gmail.com>
2022-05-19 00:04:40 +0800

    config for ScanIndel

--- index.sh
+++ index.sh
@@ -5,7 +5,7 @@ HLAs=(A B C DPA1 DPB1 DQA1 DQB1 DRB1)
 dir=$(cd `dirname $0`; pwd)
 for hla in ${HLAs[@]}; do
     config_file=$dir/db/HLA/HLA_${hla}.config.txt
-    echo bwa=$dir/db/HLA/HLA_${hla}/HLA_${hla}.fa >>$config_file
+    echo bwa=$dir/db/HLA/HLA_${hla}/HLA_${hla}.fa >$config_file
     echo freebayes=$dir/db/HLA/HLA_${hla}/HLA_${hla}.fa >>$config_file
     echo blat=$dir/db/HLA/HLA_${hla}/ >>$config_file
 done
