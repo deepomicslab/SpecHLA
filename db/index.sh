8034d8e31b575eb745827ff4144c8c23ba2102d2
WANG Shuai <wshuai294@gmail.com>
2022-05-18 17:35:18 +0800

    revise install

---
+++ db/index.sh
@@ -0,0 +1,7 @@
+
+# index the database
+../bin/novoindex  -k 14 -s 1 ref/hla_gen.format.filter.extend.DRB.no26789.ndx \
+ref/hla_gen.format.filter.extend.DRB.no26789.fasta
+
+../bin/novoindex  -k 14 -s 1 ref/hla_gen.format.filter.extend.DRB.no26789.v2.ndx \
+ref/hla_gen.format.filter.extend.DRB.no26789.v2.fasta
