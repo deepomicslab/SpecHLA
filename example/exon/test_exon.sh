a672fe465c186374ec2e5443e67d28791e33f607
WANG Shuai <wshuai294@gmail.com>
2022-05-18 14:25:49 +0800

    use local python

--- example/exon/test_exon.sh
+++ example/exon/test_exon.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-bash ../../script/exon/HLAPro_exon.sh -n NA19141 -1 ./NA19141_1.filter.fastq.gz -2 ./NA19141_2.filter.fastq.gz -p Black
+bash ../../script/exon/SpecHLA_exon.sh -n NA19141 -1 ./NA19141_1.filter.fastq.gz -2 ./NA19141_2.filter.fastq.gz -p Black
