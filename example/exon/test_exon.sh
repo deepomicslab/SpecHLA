22d8b6eb458586a4b30cfb3460b1dd514f428630
WANG Shuai <wshuai294@gmail.com>
2022-06-15 21:56:11 +0800

    revise readme and delete SpecHLA_whole.sh

--- example/exon/test_exon.sh
+++ example/exon/test_exon.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-bash ../../script/exon/SpecHLA_exon.sh -n NA19141 -1 ./NA19141_1.filter.fastq.gz -2 ./NA19141_2.filter.fastq.gz -p Black
+bash ../../script/whole/SpecHLA.sh -u 1 -n NA19141 -1 ./NA19141_1.filter.fastq.gz -2 ./NA19141_2.filter.fastq.gz -p Black
--- example/pacbio/run_pacbio.sh
+++ example/pacbio/run_pacbio.sh
@@ -1,2 +1,2 @@
 sample=child_1
-../../script/whole/SpecHLA_whole.sh -n $sample -1 ../hic/${sample}_cov10.bwa.read1.fastq.gz -2 ../hic/${sample}_cov10.bwa.read2.fastq.gz -t $sample.fastq.gz
+../../script/whole/SpecHLA.sh -n $sample -1 ../hic/${sample}_cov10.bwa.read1.fastq.gz -2 ../hic/${sample}_cov10.bwa.read2.fastq.gz -t $sample.fastq.gz
