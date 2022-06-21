d496c99622915846ba5233083c70b51bf893cdb8
WANG Shuai <wshuai294@gmail.com>
2022-06-21 20:33:43 +0800

    add typing results to cal.hla.copy.pl

--- script/cal.hla.copy.pl
+++ script/cal.hla.copy.pl
@@ -4,25 +4,34 @@ use Cwd qw/abs_path/;
 use Getopt::Long;
 
 
-my ($purity, $ploidy, $filelist, $outdir);
+my ($purity, $ploidy, $filelist, $tfile, $outdir);
 my $Help;
 GetOptions(
 	   "purity=f"  =>      \$purity,
            "ploidy=f"  =>      \$ploidy,
 	   "F=s"       =>      \$filelist,
+           "T=s"       =>      \$tfile,  ## typing result
 	   "O=s"       =>      \$outdir,
 	   "h"         =>      \$Help
 );
 
 
-#my ($purity, $ploidy) = (0.4, 1);
-# my $filelist:  Sample\tHLA_A\tHLA_A_freq.txt\n
-#
 my @hlas = ("A","B","C","DPA1","DPB1","DQA1","DQB1","DRB1");
+my %hasha;
+open RIN, "$tfile" or die "$!\n";
+my $line1 = <RIN>; my $line2 = <RIN>;
+chomp $line1; chomp $line2;
+my @array1 = split /\t/,$line1;
+my @array2 = split /\t/,$line2;
+for(my $i=1;$i<=$#array1;$i++){
+     my $id = $array1[$i];
+     my $k = (split /_/,$id)[1];
+     my $alle = $array2[$i];
+     $hasha{$k} .= "$alle\t";
+}
 
 open OUT, ">$outdir/merge.hla.copy.txt";
-#my $head = join("\t",@hlas);
-print OUT "Sample\tHLA\tcopy_number\n";
+print OUT "Sample\tHLA\tAllele1\tAllele2\tcopyratio\tKeptHLA\tLossHLA\n";
 
 open IN, "$filelist" or die "$!\tfilelist\n";
 while(<IN>){
@@ -30,15 +39,17 @@ while(<IN>){
 	my $file = $_;
 	my ($sample, $hla) = (split /\//,$file)[-2,-1];
 	$hla =~ s/_freq\.txt$//;
+        $hla =~ s/HLA_//;
 	open LI, "$file" or die "$!\tfile\n";
 	<LI>;
 	my $line1 = <LI>; my $line2 = <LI>;
-	chomp $line1; chomp $line2;
+	chomp $line1; chomp $line2;close LI;
 	my $fre1 = (split /\s+/, $line1)[1];
 	my $fre2 = (split /\s+/, $line2)[1];
-        print OUT "$sample\t$hla";
-        my $tfre;
-	if($fre1 ==0 || $fre2 ==0){$ploidy = sprintf("%.0f",$ploidy); print OUT "\t$ploidy:0";}
+        print OUT "$sample\t$hla\t$hasha{$hla}";
+        my $tfre; my $tag=0;
+        if($fre1 ==0){$ploidy = sprintf("%.0f",$ploidy); print OUT "0:$ploidy";}
+	if($fre2 ==0){$ploidy = sprintf("%.0f",$ploidy); print OUT "$ploidy:0";}
 	else{
 		     if($fre1 > $fre2){$tfre = $fre1 / $fre2}
 		     else{$tfre = $fre2 / $fre1}
@@ -55,10 +66,13 @@ while(<IN>){
 		     #}
 		     $a = sprintf("%.0f",$a);
 		     $b = sprintf("%.0f",$b);
-		     print OUT "\t$a:$b";
+		     if($fre1 > $fre2){print OUT "$a:$b"; $tag =2}
+                     else{print OUT "$b:$a"; $tag=1}
 	}
-	
-	print OUT "\n";
+     	my ($hla1,$hla2) = (split /\t/,$hasha{$hla})[0,1];
+        if($tag ==0 ){print OUT "\t$hla1\thomogeneous\n"}
+	if($tag ==1 ){print OUT "\t$hla2\t$hla1\n"}
+        if($tag ==2 ){print OUT "\t$hla1\t$hla2\n"}
 }
 close IN;
 close OUT;
@@ -74,16 +88,22 @@ sub para_alert{
 	elsif(!$filelist || !-e $filelist){
 		$alert = 1;
 	}
-	elsif(!$outdir || !-e $outdir){															  $alert = 1;																    }
+        elsif(!$tfile || !-e $tfile){
+                $alert = 1;
+        }
+	elsif(!$outdir || !-e $outdir){
+                $alert = 1;
+        }
         die "
 	USAGE:
-	perl $0 [Options] -purity <Purity> -ploidy <Ploidy> -F <filelist> -O <outdir>
+	perl $0 [Options] -purity <Purity> -ploidy <Ploidy> -F <filelist> -T <hla.result.txt> -O <outdir>
 	
 	OPTIONS:									     
      	-purity  [f]  the purity of tumor sample. <required>
         -ploidy  [f]  the ploidy of tumor sample in HLA gene region. <required>	
 	-F       [s]  the filelist. <required>															     Format, separated by table:  Sample/HLA_A_freq.txt				
-	-O       [s]  the output dir. <required>
+	-T       [s]  the hla typing result file of Spechla <required>
+        -O       [s]  the output dir. <required>
        	\n" if($alert);
 }
 
