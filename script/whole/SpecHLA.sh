1157f0180b958ac111e88d1af5c95d1174f064e8
WANG Shuai <wshuai294@gmail.com>
2022-07-08 17:12:58 +0800

    use somewhat similar sequences if no hit in the haplotype assessment

--- script/whole/SpecHLA.sh
+++ script/whole/SpecHLA.sh
@@ -126,7 +126,7 @@ mkdir -p $outdir
 group='@RG\tID:'$sample'\tSM:'$sample
 echo use ${num_threads:-5} threads.
 
-:<<!
+# :<<!
 # ################ remove the repeat read name #################
 python3 $dir/../uniq_read_name.py $fq1 $outdir/$sample.uniq.name.R1.gz
 python3 $dir/../uniq_read_name.py $fq2 $outdir/$sample.uniq.name.R2.gz
@@ -199,14 +199,13 @@ fi
 # #####################################################################################################
 
 
-!
 #################### assign long reads to gene ###################
-# if [ ${tgs:-NA} != NA ];then
-#     python3 $dir/../long_read_typing.py -r ${tgs} -n $sample -m 0 -o $outdir -j ${num_threads:-5} -a pacbio
-# fi
-# if [ ${nanopore_data:-NA} != NA ];then
-#     python3 $dir/../long_read_typing.py -r ${nanopore_data} -n $sample -m 0 -o $outdir -j ${num_threads:-5} -a nanopore
-# fi
+if [ ${tgs:-NA} != NA ];then
+    python3 $dir/../long_read_typing.py -r ${tgs} -n $sample -m 0 -o $outdir -j ${num_threads:-5} -a pacbio
+fi
+if [ ${nanopore_data:-NA} != NA ];then
+    python3 $dir/../long_read_typing.py -r ${nanopore_data} -n $sample -m 0 -o $outdir -j ${num_threads:-5} -a nanopore
+fi
 
 
 
@@ -282,7 +281,6 @@ done
 # ##################################################################################################
 
 
-!
 # ############################ annotation ####################################
 echo start annotation...
 # perl $dir/annoHLApop.pl $sample $outdir $outdir 2 $pop
