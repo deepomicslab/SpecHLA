a6c8f4435ac1dce15dacbd7bad4c3a629b2ed307
WANG Shuai <wshuai294@gmail.com>
2022-08-18 19:58:50 +0800

    use full path for some tools

--- example/exon/test_exon.sh
+++ example/exon/test_exon.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-bash ../../script/whole/SpecHLA.sh -u 1 -n NA19141 -1 ./NA19141_1.filter.fastq.gz -2 ./NA19141_2.filter.fastq.gz -p Black
+bash ../../script/whole/SpecHLA.sh -u 1 -n NA06985 -1 ./NA06985_1.filter.fastq.gz -2 ./NA06985_2.filter.fastq.gz
--- script/whole/map_block2_database.py
+++ script/whole/map_block2_database.py
@@ -14,11 +14,12 @@ import os
 
 def blast_map(fragment1):
     command = f"""
-    samtools faidx {hla_ref} {fragment1}  | bcftools consensus -H 1 {vcf} >{outdir}/{fragment1}_hap1.fasta
-    samtools faidx {hla_ref} {fragment1}  | bcftools consensus -H 2 {vcf} >{outdir}/{fragment1}_hap2.fasta
+    bin={sys.path[0]}/../../bin/
+    $bin/samtools faidx {hla_ref} {fragment1}  | $bin/bcftools consensus -H 1 {vcf} >{outdir}/{fragment1}_hap1.fasta
+    $bin/samtools faidx {hla_ref} {fragment1}  | $bin/bcftools consensus -H 2 {vcf} >{outdir}/{fragment1}_hap2.fasta
 
-    blastn -query {outdir}/{fragment1}_hap1.fasta -out {outdir}/{fragment1}_hap1.fasta.out -subject {gene_db} -outfmt 6 -max_target_seqs 10000 -strand plus
-    blastn -query {outdir}/{fragment1}_hap2.fasta -out {outdir}/{fragment1}_hap2.fasta.out -subject {gene_db} -outfmt 6 -max_target_seqs 10000 -strand plus
+    $bin/blastn -query {outdir}/{fragment1}_hap1.fasta -out {outdir}/{fragment1}_hap1.fasta.out -subject {gene_db} -outfmt 6 -max_target_seqs 10000 -strand plus
+    $bin/blastn -query {outdir}/{fragment1}_hap2.fasta -out {outdir}/{fragment1}_hap2.fasta.out -subject {gene_db} -outfmt 6 -max_target_seqs 10000 -strand plus
 
     """
     os.system(command)
