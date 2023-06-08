f624657c4269cf36b50e2770f9ca50801d143021
wshuai294 <wshuai294@gmail.com>
2023-06-08 16:45:12 +0800

    some minor revision

--- example/whole/test_whole.sh
+++ example/whole/test_whole.sh
@@ -1,1 +1,1 @@
-bash  ../../script/whole/SpecHLA.sh -j 5 -n HG00733 -1 HG00733.final_extract_1.fq.gz -2 HG00733.final_extract_1.fq.gz 
+bash  ../../script/whole/SpecHLA.sh -j 5 -n HG00733 -1 HG00733.final_extract_1.fq.gz -2 HG00733.final_extract_2.fq.gz 
