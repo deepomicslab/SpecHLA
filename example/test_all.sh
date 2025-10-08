8fa69ab669485046d250d4ff8dded8aa15d06e01
Jonas Freimuth <jonas.freimuth@tron-mainz.de>
2025-10-08 14:27:58 +0200

    example: Add script running all tests

---
+++ example/test_all.sh
@@ -0,0 +1,19 @@
+#!/bin/env bash
+
+# Run all the test scripts.
+
+set -euo pipefail
+
+run_test () {
+    local test_script_path=$1
+
+    test_dir="$(dirname $test_script_path)"
+    script="$(basename $test_script_path)"
+
+    (cd $test_dir && ./$script)
+}
+
+run_test exon/test_exon.sh
+run_test hybrid/test_hybrid.sh
+run_test pacbio/test_pacbio.sh
+run_test whole/test_whole.sh
